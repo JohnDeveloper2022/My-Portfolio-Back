9bc15b35792d4a53dd6da743ca619ab6253b8b64
Johnny <jgamb.rod@gmail.com>
2023-01-22 12:15:13 -0300

    prueba java

---
+++ PrimerProyecto/src/main/java/com/yoprogramo/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.yoprogramo.primerproyecto;
+
+/**
+ *
+ * @author usuario
+ */
+public class PrimerProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int edad = 41;
+        String es = "es inteligente.";
+        
+        System.out.println("La edad de Johnny es " + edad + " y " + es);
+    }
+}
