b37da0083f6930e408bee99e8d581303df1b5b3f
Johnny <jgamb.rod@gmail.com>
2023-04-11 19:08:08 -0300

    Cambios en propiedades

--- backend/src/main/java/com/portfolio/backend/controller/SkillController.java
+++ backend/src/main/java/com/portfolio/backend/controller/SkillController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class SkillController {
     
